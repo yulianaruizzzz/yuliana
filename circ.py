58616cdfcf376dab5605116b1dc5fcb6efbc1acf
isard <isard@debian>
2024-12-09 18:45:12 +0100

    PI més exacte

--- circ.py
+++ circ.py
@@ -3,7 +3,8 @@
 # Arxiu circ.py : càlcul perímetre d'una circumferència
 
 radi = input("Radi de la circumferència? ")
-PI = 3.14
+PI = 3.14159
+#PI = 3.14
 perimetre = 2*PI*float(radi)
 print("El perímetre de la circumferència de radi {} és {}".format(radi, perimetre))
 
