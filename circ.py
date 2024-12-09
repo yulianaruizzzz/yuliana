36064be3a22ba727a4dc64aaddd150184533e991
isard <isard@debian>
2024-12-09 18:44:33 +0100

    Millora de PI

--- circ.py
+++ circ.py
@@ -3,7 +3,9 @@
 # Arxiu circ.py : càlcul perímetre d'una circumferència
 
 radi = input("Radi de la circumferència? ")
-PI = 3.14
+#PI = 3.14
+PI = 3.1416
+
 perimetre = 2*PI*float(radi)
 print("El perímetre de la circumferència de radi {} és {}".format(radi, perimetre))
 
